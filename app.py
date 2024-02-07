eefdbd88a57058a206ad97c55e705d4380dae311
NorbertGalazka <norbert1996galazka@gmail.com>
2024-02-07 13:11:04 +0100

    add a CantorOffer and Currency class

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ import os
 app = Flask(__name__)
 
 
+class Currency:
+    def __init__(self, code, name, img_path):
+        self.code = code
+        self.name = name
+        self.img_path = img_path
+
+
+class CantorOffer:
+    def __init__(self):
+        self.currencies = []
+
+    def load_currencies(self):
+        self.currencies.append(Currency('USD', 'Dolar', 'dollaro.jpg'))
+        self.currencies.append(Currency('PLN', 'Złoty', 'zloty.jpg'))
+        self.currencies.append(Currency('EURO', 'Euro', 'euro.jpg'))
+        self.currencies.append(Currency('CHF', 'Frank', 'euro.jpg'))
+        self.currencies.append(Currency('GBP', 'Funt', 'euro.jpg'))
+
+    def get_by_code(self, code):
+        for currency in self.currencies:
+            if currency.code == code:
+                return currency
+        return None
+
 
 @app.route('/')
 def index():
@@ -25,13 +49,17 @@ def cantor(currency, amount, other):
 
 @app.route('/exchange', methods=["GET", "POST"])
 def exchange():
+    offer = CantorOffer()
+    offer.load_currencies()
     if request.method == "GET":
-        return render_template('exchange.html')
+        return render_template('exchange.html', offer=offer)
     else:
         currency = request.form['currency']
         other_currency = request.form['other_currency']
         amount = request.form['amount']
-        return redirect(url_for('cantor', currency=currency, amount=amount, other=other_currency))
+        return render_template('cantor.html',
+                               currency=currency, amount=amount,
+                               other=other_currency, currency_info=offer.get_by_code(currency))
 
 
 if __name__ == "__main__":
