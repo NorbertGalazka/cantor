bc21b5c6313430f5fb038aec137fac23f8ef190f
NorbertGalazka <norbert1996galazka@gmail.com>
2024-02-07 11:22:30 +0100

    remove test functions

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ import os
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     color = 'green'
-#     style = 'italic'
-#     if 'color' in request.args:
-#         color = request.args['color']
-#     if 'style' in request.args:
-#         style = request.args['style']
-#         print(style)
-#     return f'<h1 style="color:{color}; font-style:{style};"> Hello World! </h1>'
 
 
 @app.route('/')
@@ -42,19 +32,6 @@ def exchange():
         other_currency = request.form['other_currency']
         amount = request.form['amount']
         return redirect(url_for('cantor', currency=currency, amount=amount, other=other_currency))
-
-
-@app.route('/cook_form', methods=["GET", "POST"])
-def cook_form():
-    if request.method == "GET":
-        return render_template('cook_form.html')
-    else:
-        note = request.form['note']
-        comment = request.form['comment']
-        decision = "Never"
-        if 'decision' in request.form:
-            decision = "Sure!"
-        return render_template('cook_form_result.html', note=note, comment=comment, decision=decision)
 
 
 if __name__ == "__main__":
