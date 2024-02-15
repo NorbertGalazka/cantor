3834b6243aa3c70e0cba7f89d89669874c5ec7eb
NorbertGalazka <norbert1996galazka@gmail.com>
2024-02-15 17:16:57 +0100

    adding currency exchange functions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, flash
 import requests
 import json
-
+import codecs
+import re
 
 app = Flask(__name__)
 
@@ -10,24 +11,26 @@ app.config['SECRET_KEY'] = '1234'
 
 
 class CurrencyConverter:
-    def __init__(self, currency_code: str, amount, second_currency: str):
+    def __init__(self, currency_code: str, amount, second_currency_code: str):
         self.currency_code = currency_code
         self.amount = amount
-        self.second_currency = second_currency
+        self.second_currency_code = second_currency_code
 
     def get_currency_value_in_pln(self) -> float:
         if self.currency_code == 'PLN':
             return 1.0
         response = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/a/{self.currency_code}/?format=json')
-        response = json.loads(response.text)
-        return float(response['rates'][0]['mid'] * self.amount)
+        decoded_data = codecs.decode(response.text.encode(), 'utf-8-sig')
+        response = json.loads(decoded_data)
+        return float(response['rates'][0]['mid'])
 
     def get_second_currency_value_in_pln(self) -> float:
-        if self.currency_code == 'PLN':
+        if self.second_currency_code == 'PLN':
             return 1.0
-        response = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/a/{self.second_currency}/?format=json')
-        response = json.loads(response.text)
-        return float(response['rates'][0]['mid'] * self.amount)
+        response = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/a/{self.second_currency_code}/?format=json')
+        decoded_data = codecs.decode(response.text.encode(), 'utf-8-sig')
+        response = json.loads(decoded_data)
+        return float(response['rates'][0]['mid'])
 
     def get_value(self):
         return self.get_currency_value_in_pln() / self.get_second_currency_value_in_pln() * self.amount
@@ -43,15 +46,13 @@ class Currency:
 class CantorOffer:
     def __init__(self):
         self.currencies = []
-        self.denied_codes = []
 
     def load_currencies(self):
         self.currencies.append(Currency("USD", 'Dolar', 'dollaro.jpg'))
         self.currencies.append(Currency("PLN", 'Złoty', 'zloty.jpg'))
-        self.currencies.append(Currency("EURO", 'Euro', 'euro.jpg'))
+        self.currencies.append(Currency("EUR", 'Euro', 'euro.jpg'))
         self.currencies.append(Currency('CHF', 'Frank', 'euro.jpg'))
         self.currencies.append(Currency('GBP', 'Funt', 'euro.jpg'))
-        self.denied_codes.append("USD")
 
     def get_by_code(self, code):
         for currency in self.currencies:
@@ -75,13 +76,21 @@ def exchange():
         currency = request.form['currency']
         other_currency = request.form['other_currency']
         amount = request.form['amount']
-        for currency in [currency, other_currency]:
-            if currency in offer.denied_codes:
-                flash(f'The currency {currency} cannot be accepted!')
+        pattern = r'^[\d,.]+$'
+        if re.match(pattern, amount):
+            amount = amount.replace(',', '.')
+            convert = CurrencyConverter(currency_code=currency, amount=float(amount), second_currency_code=other_currency)
+            quantity_received = convert.get_currency_value_in_pln()
+            value = round(convert.get_value(), 2)
+            print(value)
 
-        return render_template('exchange_results.html',
-                               currency=currency, amount=amount,
-                               other=other_currency, currency_info=offer.get_by_code(currency))
+            return render_template('exchange_results.html',
+                                   currency=currency, amount=amount,
+                                   other=other_currency, currency_info=offer.get_by_code(currency),
+                                   quantity_received=quantity_received, value=value)
+        else:
+            flash('Panie Kostecki nie ze mną te numery ;) Proszę wpisać jeszcze raz z poprawnymi danymi')
+            return render_template('exchange.html', offer=offer)
 
 
 if __name__ == "__main__":
