af62bc8346e924732156ec0437685fffac1f538e
NorbertGalazka <norbert1996galazka@gmail.com>
2024-03-05 12:02:57 +0100

    add email validation

--- blueprints/register/register.py
+++ blueprints/register/register.py
@@ -1,8 +1,14 @@
 from flask import flash, Blueprint, request, session, render_template, redirect, url_for
 from user_pass import UserLog
+import re
 
 
 register_blueprint = Blueprint("register", __name__, template_folder='templates')
+
+
+def is_valid_email(email_adress):
+    regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(regex, email_adress) is not None
 
 
 @register_blueprint.route('/register', methods=['GET', 'POST'])
@@ -15,8 +21,11 @@ def register_route():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        if len(password) < 4:
-            flash('password cannot be less than 4 characters long')
+        if not is_valid_email(email) or len(password) < 4:
+            if len(password) < 4:
+                flash('password cannot be less than 4 characters long')
+            else:
+                flash('Incorrect email')
             return render_template('register.html', active_menu='register',
                                    username=username, actual_user=actual_user, email=email)
         log = UserLog(username, password, email)
